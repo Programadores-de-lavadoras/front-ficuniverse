53617f8602582c0de79ec3e53fd3ba2a27bcd8e9
Stevens <u20201e735@upc.edu.pe>
2023-09-29 23:10:07 -0500

    feature: added imports modules and changed default theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import { AvatarMenuComponent } from './shared/components/avatar-menu/avatar-menu.component';
+import { NotificationMenuComponent } from './shared/components/notification-menu/notification-menu.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    AvatarMenuComponent,
+    NotificationMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
