6db14a752f602c0995e944db5e19bbd3a5fb7afc
Stevens <u20201e735@upc.edu.pe>
2023-09-30 02:00:38 -0500

    refactor: changing db.json, dependencies, and adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ChaptersComponent} from "./chapters/pages/chapters/chapters.component";
+import {HomeComponent} from "./public/pages/home/home.component";
+import {WriteComponent} from "./public/pages/write/write.component";
+import {AuthorsComponent} from "./public/pages/authors/authors.component";
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {SearchComponent} from "./public/pages/search/search.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'chapters', component: ChaptersComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'writing', component: WriteComponent},
+  {path: 'authors', component: AuthorsComponent},
+  {path: 'search', component: SearchComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,13 +15,32 @@ import {MatButtonModule} from "@angular/material/button";
 import { AvatarMenuComponent } from './shared/components/avatar-menu/avatar-menu.component';
 import { NotificationMenuComponent } from './shared/components/notification-menu/notification-menu.component';
 import {MatIconModule} from "@angular/material/icon";
+import { ChapterFormComponent } from './chapters/components/chapter-form/chapter-form.component';
+import { ChaptersComponent } from './chapters/pages/chapters/chapters.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import { FooterContentComponent } from './chapters/components/footer-content/footer-content.component';
+import { HomeComponent } from './public/pages/home/home.component';
+import { WriteComponent } from './public/pages/write/write.component';
+import { AuthorsComponent } from './public/pages/authors/authors.component';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+import { SearchComponent } from './public/pages/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     AvatarMenuComponent,
-    NotificationMenuComponent
+    NotificationMenuComponent,
+    ChapterFormComponent,
+    ChaptersComponent,
+    FooterContentComponent,
+    HomeComponent,
+    WriteComponent,
+    AuthorsComponent,
+    PageNotFoundComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +53,10 @@ import {MatIconModule} from "@angular/material/icon";
     FormsModule,
     MatButtonModule,
     MatIconModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
