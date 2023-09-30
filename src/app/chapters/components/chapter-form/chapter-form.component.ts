2de237716483837959016a6ac593d6626ab8c50b
Stevens <u20201e735@upc.edu.pe>
2023-09-30 02:02:20 -0500

    feature: adding chapter form component

---
+++ src/app/chapters/components/chapter-form/chapter-form.component.ts
@@ -0,0 +1,46 @@
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {Chapter} from "../../model/chapter.entity";
+import {NgForm} from "@angular/forms";
+import {ChaptersComponent} from "../../pages/chapters/chapters.component";
+
+@Component({
+  selector: 'app-chapter-form',
+  templateUrl: './chapter-form.component.html',
+  styleUrls: ['./chapter-form.component.css']
+})
+export class ChapterFormComponent {
+  @Input()chapter: Chapter;
+  @ViewChild('chapterForm', {static: false}) chapterForm!: NgForm;
+  @Input() editMode = false;
+  @Output()chapterAdded = new EventEmitter<Chapter>();
+  @Output()chapterUpdated = new EventEmitter<Chapter>();
+  @Output()editCanceled = new EventEmitter();
+
+  constructor() {
+    this.chapter = {} as Chapter;
+  }
+
+  private resetEditState(){
+    this.editMode = false;
+    this.chapterForm.resetForm();
+    this.chapter = {} as Chapter;
+  }
+
+  onSubmit(){
+    if(this.chapterForm.form.valid){
+      if(this.editMode){
+        this.chapterUpdated.emit(this.chapter);
+      } else{
+        this.chapterAdded.emit(this.chapter);
+      }
+      this.resetEditState();
+    } else{
+      console.log('Invalid Data');
+    }
+  }
+
+  onCancel(){
+    this.resetEditState();
+    this.editCanceled.emit();
+  }
+}
