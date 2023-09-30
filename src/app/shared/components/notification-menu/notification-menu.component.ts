be0b6b49a932cd3d88b07f48c2499a7e4a3b90f7
Stevens <u20201e735@upc.edu.pe>
2023-09-30 02:05:48 -0500

    feature: added placeholder for home notification, not visible yet

---
+++ src/app/shared/components/notification-menu/notification-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-menu',
+  templateUrl: './notification-menu.component.html',
+  styleUrls: ['./notification-menu.component.css']
+})
+export class NotificationMenuComponent {
+
+}
