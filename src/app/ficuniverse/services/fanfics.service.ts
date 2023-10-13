5694526925fa48453d7e89401cb4686738fec4ae
Stevens <u20201e735@upc.edu.pe>
2023-10-13 09:42:11 -0500

    feat: merging LeonardoDueñas and adding functionality to the navbar

---
+++ src/app/ficuniverse/services/fanfics.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from 'src/app/shared/services/base.service';
+import { Fanfic } from '../model/fanfic.entity';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FanficsService extends BaseService<Fanfic>{
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/fanfics'
+   }
+}
