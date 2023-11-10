fa8577c9a1b6f7f40e3692f4578a2efed158b895
Stevens <u20201e735@upc.edu.pe>
2023-11-10 00:04:08 -0500

    feat: implementing filters and sorting for search results

--- src/app/ficuniverse/model/fanfic.entity.ts
+++ src/app/ficuniverse/model/fanfic.entity.ts
@@ -1,23 +1,39 @@
+import {Category} from "./category.entity";
+import {Tag} from "./tag.entity";
+import {Chapter} from "../../chapters/model/chapter.entity";
+
 export class Fanfic {
     id: number;
     title: string;
     author: string;
     summary: string;
     publication_date: string;
+    date: Date;
     language_id: number;
     status_id: number;
     saga_id: number;
+    views: number;
+    favorites: number;
     thumbnail: string;
+    chapters: Chapter[];
+    categories: Category[];
+    tags: Tag[];
     constructor(){
         this.id = 0;
         this.title = '';
         this.author = '';
         this.summary = '';
         this.publication_date = '';
+        this.date = new Date();
         this.language_id = 0;
         this.status_id = 0;
         this.saga_id = 0;
+        this.views = 0;
+        this.favorites = 0;
         this.thumbnail = '';
+        this.chapters = [];
+        this.categories = [];
+        this.tags = [];
     }
 }
 
