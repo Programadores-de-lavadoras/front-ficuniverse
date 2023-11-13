995a30aff64d250231c52b9eebafb7ff39239b18
Stevens <u20201e735@upc.edu.pe>
2023-11-13 12:59:56 -0500

    feat: adding homepage version 1

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FanficsService} from "../../../ficuniverse/services/fanfics.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  fanfics: Array<any> = []
+  constructor(private fanficService: FanficsService) {
+  }
 
+  images = [{
+    imageSrc: 'https://ma.wattpad.com/the_hookup_pact_hfc_desktop_final.jpg',
+    imageAlt: 'promo1'
+  },
+    {
+      imageSrc: 'https://ma.wattpad.com/threadofashandfire_hfc_desktop_final.jpg',
+      imageAlt: 'promo2'
+    },
+    {
+      imageSrc: 'https://ma.wattpad.com/picky_eater__hfc_desktop_final.png',
+      imageAlt: 'promo3'
+    },]
+  ngOnInit(): void {
+    this.fanficService.getAll().subscribe((response: any) =>{
+      this.fanfics = response
+    } )
+  }
 }
